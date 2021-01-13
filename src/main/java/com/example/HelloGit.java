bf337415fd7a28b822ef04bb6685823c1342aa7c
Leonhard-Euler-ai <873406454@qq.com>
2021-01-13 23:42:53 +0800

    添加文件

---
+++ src/main/java/com/example/HelloGit.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * 类描述
+ *
+ * @author <a href="mailto:873406454@qq.com">Li Hangfei</a>
+ * @date 2021/1/13
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
